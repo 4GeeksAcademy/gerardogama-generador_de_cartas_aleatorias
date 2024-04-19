d2ee0a829168c5ed1585ad455f2f8c978ec7bc16
gerardogama <153300914+gerardogama@users.noreply.github.com>
2024-04-19 21:13:05 +0000

    card completed

--- src/app.js
+++ src/app.js
@@ -6,6 +6,18 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".card").classList.add(generateRandomIcon());
+  document.querySelector(".number").innerHTML = generateRandomNumber();
+};
+
+let generateRandomIcon = () => {
+  let Icon = ["spade", "diamond", "club", "heart"];
+  let randomIcon = Math.floor(Math.random() * Icon.length);
+  return Icon[randomIcon];
+};
+
+let generateRandomNumber = () => {
+  let numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+  let randomNumber = Math.floor(Math.random() * numbers.length);
+  return numbers[randomNumber];
 };
